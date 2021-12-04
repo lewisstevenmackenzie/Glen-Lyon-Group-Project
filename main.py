01440e0bd21105334df266bbab2f9ded03703938
Lewis Mackenzie <lewisstevenmackenzie@gmail.com>
2021-12-04 00:44:37 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,104 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+from flask_login import LoginManager, login_user
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8656d430a91e21e849364b1dadde70eb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='athlete', lazy=True)
+
+    def __repre__(self):
+        return f"User('{self.username}', '{self.email}', '{self.profile_image}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+
+posts = [
+        {
+            'athlete': 'Lewis Mackenzie',
+            'title': 'Run',
+            'content': 'I ran 5km',
+            'date':'July 10th 2021'
+        },
+        {
+            'athlete': 'Harry Mackenzie',
+            'title': 'Cycle',
+            'content': 'I cycled 20km',
+            'date':'July 15th 2021'
+        },
+
+        ]
+
+@app.route("/")
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title = 'about')
+
+@app.route("/error404")
+def error404():
+    return render_template('error404.html', title = 'error404')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and (user.password == form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
