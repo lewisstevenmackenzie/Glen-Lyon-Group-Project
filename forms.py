2616d0c9855920fcefba14271a3ea9e3c36c0eef
Lewis Mackenzie <lewisstevenmackenzie@gmail.com>
2021-12-04 00:48:04 +0000

    ..

---
+++ forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), Length(min=3, max = 10)])
+
+    email = StringField('email', validators=[DataRequired(), Email()])
+    
+    password = PasswordField('password', validators=[DataRequired()])
+    
+    confirm_password = PasswordField('confirm password', validators=[DataRequired(), EqualTo('password')])
+
+    profilepic = FileField(validators=[ FileRequired(), FileAllowed(['jpg'], 'Only jpg!') ])
+
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    
+    email = StringField('email', validators=[DataRequired(), Email()])
+
+    password = PasswordField('password', validators=[DataRequired()])
+
+    remember = BooleanField('Remember Me') 
+
+    submit = SubmitField('Login')
