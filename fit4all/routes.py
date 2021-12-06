93bb4e010274e27d706678a07f8fc0087a1e4362
Lewis Mackenzie <lewisstevenmackenzie@gmail.com>
2021-12-06 18:19:41 +0000

    test2

--- fit4all/routes.py
+++ fit4all/routes.py
@@ -6,7 +6,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
 def home():
-    posts = Post.query.filter_by(Post.user_id==current_user.id).all()
+    posts = Post.query.filter_by(user_id=current_user.id).all()
     return render_template('home.html', posts = posts)
 
 @app.route("/about")
