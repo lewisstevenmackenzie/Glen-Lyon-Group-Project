2a639d149ef407b4b3150728b5390e45c070aebc
Lewis Mackenzie <lewisstevenmackenzie@gmail.com>
2021-12-07 13:46:21 +0000

    update

--- fit4all/models.py
+++ fit4all/models.py
@@ -28,7 +28,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return f"Post('{self.title}', '{self.date}')"
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
