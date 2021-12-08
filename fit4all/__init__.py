9ec223cb3ef998578ba37fce50723f7abfb03b3d
lewisstevenmackenzie <64756559+lewisstevenmackenzie@users.noreply.github.com>
2021-12-08 17:14:33 +0000

    tesssst

--- fit4all/__init__.py
+++ fit4all/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, UserMixin
-from datetime import datetime
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -14,33 +13,4 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='athlete', lazy=True)
-    notes = db.relationship('Note', backref='athlete', lazy=True)
-
-    def __repre__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date}')"
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, nullable=False)
-    note_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-
-    def __repr__(self):
-        return f"Note('{self.content}')"
+from fit4all import routes
