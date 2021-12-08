c0e65f30dd48020262706fc3da4bb7ab0b281716
lewisstevenmackenzie <64756559+lewisstevenmackenzie@users.noreply.github.com>
2021-12-08 22:26:51 +0000

    added gpx file to post

--- fit4all/models.py
+++ fit4all/models.py
@@ -26,6 +26,7 @@ class Post(db.Model):
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    gpx_file = db.Column(db.String(20), nullable=True)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date}')"
