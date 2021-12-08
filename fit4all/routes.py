bbd1038cc62d9c4730254e738629aef7b12de364
lewisstevenmackenzie <64756559+lewisstevenmackenzie@users.noreply.github.com>
2021-12-08 21:40:46 +0000

    dd

--- fit4all/routes.py
+++ fit4all/routes.py
@@ -210,4 +210,4 @@ def upload_file():
         current_user.image_file = f.filename
         db.session.commit()
         return render_template("home.html")
-    return render_template("home.html")
+    return render_template("upload_profile_pic.html")
