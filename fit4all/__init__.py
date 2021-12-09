db37b5aed9c2f17e7ed00415830393966d5c1aa0
lewisstevenmackenzie <64756559+lewisstevenmackenzie@users.noreply.github.com>
2021-12-09 16:56:09 +0000

    fixed

--- fit4all/__init__.py
+++ fit4all/__init__.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -16,43 +15,4 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
 
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='athlete', lazy=True)
-    notes = db.relationship('Note', backref='athlete', lazy=True)
-
-    def __repre__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    activity_type = db.Column(db.String(100), nullable=False)
-    distance = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    gpx_file = db.Column(db.String(20), nullable=True)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date}')"
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text, nullable=False)
-    note_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-
-    def __repr__(self):
-        return f"Note('{self.content}')"
+from fit4all import routes
