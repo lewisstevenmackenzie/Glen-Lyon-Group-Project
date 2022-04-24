b7a8674e076ae279e770bef03253ea2f7fc1f89a
Lewis <lewisstevenmackenzie@gmail.com>
2022-04-24 23:59:06 +0100

    Bug Fix

--- TestEnv/bin/activate.fish
+++ TestEnv/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/lewis/GlenLyon/SET09103Coursework/TestEnv'
+set -gx VIRTUAL_ENV '/home/lewis/Glen-Lyon-Group-Project/TestEnv'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
